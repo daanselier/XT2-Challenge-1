c32483c6deb00352827a9086ad75e3fef175bf3f
Daan <daanselier@users.noreply.github.com>
2021-02-26 15:45:03 +0100

    Middagsessie Vrijdag 26 Februari
    Kleine toevoegingen plus nieuwe bestandsindeling voor de css bestanden

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+Chart.defaults.global.defaultFontColor = 'white';
+Chart.defaults.global.defaultFontFamily = 'Roboto, sans-serif';
+
 var ctx = document.getElementById('fuelChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'line',
@@ -8,21 +11,15 @@ var myChart = new Chart(ctx, {
             data: [100, 80, 76, 72, 69, 63, 58, 55],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 173, 173, 1)',
+                'rgba(255, 214, 165, 1)',
+                'rgba(253, 255, 182, 1)',
+                'rgba(202, 255, 191, 1)',
+                'rgba(155, 246, 255, 1)',
+                'rgba(160, 196, 255, 1)',
+                'rgba(189, 178, 255, 1)'
             ],
             borderWidth: 1
         }]
@@ -35,5 +32,28 @@ var myChart = new Chart(ctx, {
                 }
             }]
         }
+    }
+});
+
+var ctx = document.getElementById('distanceChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Distance to go (million miles)', 'Distance travelled (million miles)'],
+        datasets: [{
+            label: 'Fuel left in %',
+            data: [20, 120],
+            backgroundColor: [
+                'rgba(160, 196, 255, 1)',
+                'rgba(189, 178, 255, 1)'
+            ],
+            borderColor: [
+                'rgba(160, 196, 255, 1)',
+                'rgba(189, 178, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
     }
 });
