237f7eecfd61f1e97ac33b5a6fa797b4fe7a6a21
Daan <daanselier@users.noreply.github.com>
2021-02-26 13:16:29 +0100

    Ochtendsessie Vrijdag 26 Februari
    -Toevoegen van benzineverbruik chart
    -SpaceX logo toegevoegd voor wat extra space 🚀
    -Style binnen de css meer aan SpaceX gekoppeld om Elon Musk warm te maken
    *background-color: black
    *color:white
    *width en height van het SpaceX logo aangepast

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,39 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
+var ctx = document.getElementById('fuelChart').getContext('2d');
+var myChart = new Chart(ctx, {
     type: 'line',
-
-    // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['0 miles', '20 million miles', '40 million miles', '60 million miles', '80 million miles', '100 million miles', '120 million miles'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            label: 'Fuel left in %',
+            data: [100, 80, 76, 72, 69, 63, 58, 55],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderWidth: 1
         }]
     },
-
-    // Configuration options go here
-    options: {}
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
